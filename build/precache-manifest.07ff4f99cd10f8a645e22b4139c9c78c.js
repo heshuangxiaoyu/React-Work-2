4c2ddbdbfc8ad7a1a709bcafbac4f3e122812bd2
贺爽 <45160925+heshuangxiaoyu@users.noreply.github.com>
2020-10-18 20:30:54 +0800

    Add files via upload

---
+++ build/precache-manifest.07ff4f99cd10f8a645e22b4139c9c78c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f26be880ea219dc57bbf6fb693aa703",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34fd45e05109fd916ee3",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "37f2c4cc08f137a83866",
+    "url": "/static/css/main.9aadfbb1.chunk.css"
+  },
+  {
+    "revision": "34fd45e05109fd916ee3",
+    "url": "/static/js/2.a8a78d93.chunk.js"
+  },
+  {
+    "revision": "de5fa9cfc3624bea3d7565f048bfaaaf",
+    "url": "/static/js/2.a8a78d93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37f2c4cc08f137a83866",
+    "url": "/static/js/main.a7adc5b9.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
